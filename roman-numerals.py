11b75724ee4780d5cab8e36f619fc84a5b55a458
Istvan Beregszaszi <beistvan@gmail.com>
2025-08-09 14:39:45 +0200

    Roman Numerals

---
+++ roman-numerals.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n == 1:
+    print("I")
+elif n == 5:
+    print("V")
+elif n == 10:
+    print("X")
+elif n == 50:
+    print("L")
+elif n == 100:
+    print("C")
+elif n == 500:
+    print("D")
+elif n == 1000:
+    print("M")
